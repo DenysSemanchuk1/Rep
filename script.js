b99b52ca7f27ab12fb2f48839adf2094b789c583
Denys Semanchuk <semanchukdenys@gmail.com>
2022-02-01 21:06:10 +0200

    project preparation

---
+++ script.js
@@ -0,0 +1,17 @@
+let money = prompt("Ваш бюджет на месяц?"),
+	time = prompt("Введите дату в формате YYYY-MM-DD");
+let appData = {
+	budgetData: money,
+	timeData: time,
+	expenses: {},
+	optionalExpenses : {},
+	income: [],
+	savings: false,
+};
+let a1 = prompt("Введите обязательную статью расходов в этом месяце", ''),
+	a2 = prompt("Во сколько обойдется?", ''),
+	a3 = prompt("Введите обязательную статью расходов в этом месяце", ''),
+	a4 = prompt("Во сколько обойдется?", '');
+appData.expenses.a1 = a1;
+appData.expenses.a3 = a3;
+alert(appData.budgetData / 30);
